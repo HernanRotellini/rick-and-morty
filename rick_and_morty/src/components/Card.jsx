f6d86659daf9a2a99e095122c0c3efb8be5c7c30
Hernan Rotellini <hernanrotellini@hotmail.com>
2023-02-20 12:34:26 -0300

    actualizacion css basico

--- rick_and_morty/src/components/Card.jsx
+++ rick_and_morty/src/components/Card.jsx
@@ -1,7 +1,10 @@
+import s from "./card.module.css"
 export default function Card(props) {
    return (
       <div>
-          <button onClick={props.onClose}>X</button>
+          <button className={s.xbutton} onClick={props.onClose}>X
+          <span>Al hacer click aqui eliminara el personaje</span>
+          </button>
          <h2>{props.name}</h2>
          <h2>{props.species}</h2>
          <h2>{props.gender}</h2>

d473e89ac2a94ba831c4f8051a6b2ad2e084b636
Hernan Rotellini <hernanrotellini@hotmail.com>
2023-02-24 15:57:55 -0300

    Login y estilos de fondo

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -5,18 +5,34 @@ import Nav from './components/Nav';
 import Cards from './components/Cards';
 import About from './components/About';
 import Detail from './components/Detail';
-
-
+import Form from './components/Form';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const URL = "https://be-a-rym.up.railway.app/api"
 const APIKEY= "3681102e3393.a2e00517a5401fef1d76"
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
   const [characters, setCharacters] = useState([]);
- 
+  const [access, setAccess]= useState(false);
+
+  const username = 'Admin@Admin.com';
+const password = 'Admin123';
   //https://be-a-rym.up.railway.app/api/character/162?key=3681102e3393.a2e00517a5401fef1d76
   const onClose = (id) => {
     setCharacters(characters.filter((c) => c.id !== id));
   }
+ 
+  function login(userData) {
+    if (userData.password === password && userData.username === username) {
+       setAccess(true);
+       navigate('/home');
+    }
+ }
+  useEffect(() => {
+    !access && navigate('/');
+ }, [access]);
 
   function onSearch(character) {
     fetch(`${URL}/character/${character.id}?key=${APIKEY}`)
@@ -34,17 +50,26 @@ function App() {
         }
        });
  }
-  return (
-   <div class="App">
-    <Nav onSearch={onSearch}/>
-    <Routes>
-    <Route exact path="/about" element={<About/>} />
-    <Route class="grid" exact path="/home" element={<Cards characters={characters} onClose={onClose} />}>
-      
-      </Route>
-      <Route path="/detail/:detailId" element={<Detail/>} />
 
-      </Routes>
+ if (location.pathname === '/') {
+ return (
+ <div className="App form">
+  <h1>Bienvenido al Mundo de Rick and Morty</h1>
+  <Form login={login}/>
+  
+ </div>)
+}
+  return (
+   <div className="App">
+    <Nav onSearch={onSearch}/>
+   
+  <Routes>
+
+<Route exact path="/about" element={<About/>} />
+<Route className="grid-container" path="/home" element={<Cards characters={characters} onClose={onClose} />}></Route>
+<Route path="/detail/:detailId" element={<Detail/>} />
+
+ </Routes>
     
       
     </div>
--- rick_and_morty/src/components/Card.jsx
+++ rick_and_morty/src/components/Card.jsx
@@ -7,19 +7,17 @@ export default function Card(props) {
    };
    
    return (
-     
-      <div className={s.cartas} >
-         <button className={s.xbutton} onClick={handleClick}>
-            X
-            <span>Cerrar la tarjeta del personaje</span>
-         </button>
-         <Link to={`/detail/${props.id}`}>
+      <div className={s.cartas}>
+         <button className={s.xbutton} onClick={handleClick}>X</button>
+      <Link to={`/detail/${props.id}`}>
          <h2>{props.name}</h2>
          </Link>
-         <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
-         <img src={props.image} alt="" /> 
+      <h2>{props.species}</h2>
+      <h2>{props.gender}</h2>
+      <div className={s.image}>
+        <img src={props.image} alt="" />
       </div>
       
+    </div>
    );
 }

0e010ddb4fa9454ba83659905a7aadfef138da33
Hernan Rotellini <hernanrotellini@hotmail.com>
2023-03-10 11:26:03 -0300

    Update About.jsx

--- rick_and_morty/src/components/About.jsx
+++ rick_and_morty/src/components/About.jsx
@@ -4,7 +4,7 @@
     return (
         <div className='about'>
             
-            <h1>Creador: </h1>
+           
             <p>Este proyecto trata sobre la serie Rick and Morty fue creado por  utilizando Javascript Node.js React y Redux
                 el mismo realiza llamadas a un API externa en la cual se encuentran los personajes de dicha serie
             </p>
